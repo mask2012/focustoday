0b9956b39bc04770d97f18d34b0b2621dce9b254
mask2012 <156544634@qq.com>
2020-05-06 20:41:05 +0800

    今日进度：完成todo list自动大小 , 时钟

--- README.md
+++ README.md
@@ -7,15 +7,16 @@ a chrome crx main focus on todo list everyday
 ## 需求分析  
 **基本需求**  
 1. 初次进入引导，询问名字
-2. 时间展示 ✓ui
+2. 时间展示 ✓
 3. 问候语 ✓ui
 4. 背景图一日一换 ✓
 
 **高级需求**  
-1. todoList  ✓ ui
+1. todoList  ✓
 2. 格言一日一换
 3. 常用工具 ★★
 4. 天气播报
+5. 白屏页
 
 
 ## 开发过程记录  
@@ -54,11 +55,67 @@ vue项目关联github仓库还挺坑，没法用git小乌龟完成，步骤如�
 ![](https://cdn.jsdelivr.net/gh/mask2012/imgBed/20200430091937.png)
 
 
-突发奇想我可以把todo 列表做成如果条数多就字大，条数少就字小，总之会去适配
+**突发奇想我可以把todo 列表做成如果条数多就字大，条数少就字小**    
+尝试了直接用display:flex发现并不会无限制缩小行高，而且在结合overflow:scroll的时候，顶部会出现断头，这可能就是flex造成的bug了，所以这条路断了  
+另一条路就是在resize window的时候实时获取todo列表的高度，再结合todo列表条数，得到每条高度和字号，这样就可以条数越多字越小了，而且保证不会出现滚动条
+![](https://cdn.jsdelivr.net/gh/mask2012/imgBed/1.gif)
 
-以后也可以弄一个番茄工作计时，到时提醒的功能
+
+**以后也可以弄一个番茄工作计时，到时提醒的功能**  
 ![](https://cdn.jsdelivr.net/gh/mask2012/imgBed/20200430162255.png)
 
 
+**修饰一下滚动条的style**  
+https://www.cnblogs.com/vicky-li/p/css.html
 
+
+
+
+**设计logo**
+花了半个小时就搞定了logo，不得不说，中文logo制作的都是一坨屎  
+google搜logo generator出来的结果是真心牛逼，不过竟然是收费的，好在小图我也勉强够用了  
+https://editor.freelogodesign.org/?lang=en&companyname=focus&category=0  
+https://www.brandcrowd.com/maker/logo/43b1190b-1b7c-4842-be79-443a9265c73e?text=focustoday  
+
+
+用在线ps简单处理了一下  
+https://ps.gaoding.com/#/
+
+最终logo长成这样  
+![](https://cdn.jsdelivr.net/gh/mask2012/imgBed/focus_logo.png)
+
+用在线facicon生成一下即可
+https://www.favicon-generator.org/
+
+
+**发现壁纸需要的是landscape类的图片，发现momentum使用的是unsplash提供的服务**    
+官方说不限流量，只需注册开发者即可，看起来很不错的样子
+https://unsplash.com/developers  
+
+又看了一会，发现他的api是不收费的  
+demo版的限制是一小时50次；高级版一小时5k次，你只需要按照他的要求使用，比如必须注明图片源于unsplash，写明作者，等等等等
+https://help.unsplash.com/en/articles/3887917-when-should-i-apply-for-a-higher-rate-limit
+
+接下来是unsplash的使用方法
+https://github.com/unsplash/unsplash-js
+
+调用api后发现下载还是挺慢的，也没法直接满足我一日换一张的需求  
+使用unsplash的api能实现的就是2种思路  
+一种是search关键字，一种是手动加入collection，然后请求时带上page，实现这个page就会比较麻烦  
+
+由此发现了好多图片站  
+国外的  
+https://www.liaosam.com/free-hd-photo-stocks.html
+国内的  
+https://www.zhihu.com/question/265481379
+
+甚至还发现了用爬虫的方法
+https://blog.csdn.net/qq_41536331/article/details/81028749  
+
+目前我想到的最好的方法应该是
+1. 爬虫爬取照片
+2. 照片上传github，用jsdelivr加速访问
+3. 启动后台服务，每日更新图片地址，前端直接请求此服务，得到图片url
+4. 为了保证图片不会占满github仓库，只需保留7张图片，定时删除无用的图片
+5. 保证爬取，上传，更新配置，删除无用图片流程不间断自动执行
 
--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,37 @@
   <div id="app">
     <div class="background_holder">
       <span class="bg_shadow fadein"></span>
-      <span class="bg_img fadein"></span>
+      <span class="bg_img fadein" :style="'background:url('+bgImg+') center center/cover no-repeat'"></span>
     </div>
 
     <div class="region region_center">
-      <div class="time">16:27</div>
+      <div class="time">{{currentTime}}</div>
       <div class="greeting">Good morning, Mask.</div>
     </div>
 
     <div class="region region_center_below">
       <div class="todo_list_holder">
         <div class="todo_list">
-          <h3>What is your main focus for today?</h3>
-          <input
-            type="text"
-            autocomplete="off"
-            maxlength="30"
-            v-model="newTodo"
-            @keyup.enter="addTodo"
-          />
-          <div class="todo_scroll">
-            <ul v-show="todos.length" v-cloak>
-              <li v-for="todo in todos" :key="todo.id" :class="{ li_done: todo.completed}">
-                <span @click="toggleTodo(todo)">{{ todo.title }}</span>
+          <div class="todo_input">
+            <h3>What is your main focus for today?</h3>
+            <input
+              type="text"
+              autocomplete="off"
+              maxlength="35"
+              v-model="newTodo"
+              @keyup.enter="addTodo"
+            />
+          </div>
+          <div class="todo_scroll" v-show="todos.length" v-cloak>
+            <ul :style="'height:'+listHeight+'px'">
+              <li
+                v-for="todo in todos"
+                :key="todo.id"
+                :class="{ li_done: todo.completed}"
+                :style="'height:'+liHeight+'px; line-height:'+liHeight+'px; font-size:'+liFontSize+'px'"
+                @click="toggleTodo(todo)"
+              >
+                <span>{{ todo.title }}</span>
                 <i class="ico_destroy" @click="removeTodo(todo)"></i>
               </li>
             </ul>
@@ -55,6 +63,31 @@ var todoStorage = {
   }
 };
 
+import moment from "moment";
+import Unsplash, { toJson } from "unsplash-js";
+import fetch from "node-fetch";
+global.fetch = fetch;
+
+const unsplash = new Unsplash({
+  accessKey: "Hr-gc8bEf0AnAC0XRo1WUNvP9oCw4A9ddAOdhYzJ674",
+  secret: "44WWENgCIrtD-H-y1xTPK90IaN_vaTeihYxpgoJBD_0",
+  headers: {
+    "X-Custom-Header": "mask2012"
+  },
+  timeout: 5000 // values set in ms
+});
+// unsplash.users.profile("naoufal")
+//   .catch(err => {
+//     // Your flawless error handling code
+//     console.log('unsplash error goes here',err);
+//   });
+
+// unsplash.search.photos("dogs", 1, 10, { orientation: "portrait" })
+//   .then(toJson)
+//   .then(json => {
+//     console.log('json',json);
+//     // Your code
+//   });
 
 export default {
   name: "App",
@@ -63,23 +96,27 @@ export default {
   },
   data() {
     return {
-      todos: todoStorage.fetch(),
-      newTodo: "",
-      editedTodo: null,
-      visibility: "all"
+      todos: todoStorage.fetch(), //todo全集
+      newTodo: "", //新的todo
+      listHeight: document.documentElement.clientHeight * 0.2,
+      liHeight: 0,
+      liFontSize: 10,
+      currentTime: moment().format("HH:mm"),
+      bgImg: ""
     };
   },
-  // watch todos change for localStorage persistence
   watch: {
     todos: {
       handler: function(todos) {
         todoStorage.save(todos);
+        this.liHeight = this.listHeight / todos.length;
+        this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
+        this.liFontSize =
+          this.liFontSize > 40 ? 40 + this.liFontSize / 10 : this.liFontSize;
       },
       deep: true
     }
   },
-  // methods that implement data logic.
-  // note there's no DOM manipulation here at all.
   methods: {
     addTodo: function() {
       var value = this.newTodo && this.newTodo.trim();
@@ -99,19 +136,38 @@ export default {
       this.todos.splice(this.todos.indexOf(todo), 1);
     },
     toggleTodo: function(todo) {
-      const currentIndex=this.todos.indexOf(todo)
-      this.todos[currentIndex].completed=!this.todos[currentIndex].completed
-    },
-
-
+      const currentIndex = this.todos.indexOf(todo);
+      this.todos[currentIndex].completed = !this.todos[currentIndex].completed;
+    }
   },
   mounted() {
-    
+    this.liHeight = this.listHeight / this.todos.length;
+    this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
+
+    window.onresize = () => {
+      this.listHeight = document.documentElement.clientHeight * 0.2;
+      this.liHeight = this.listHeight / this.todos.length;
+      this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
+      this.liFontSize =
+        this.liFontSize > 40 ? 40 + this.liFontSize / 10 : this.liFontSize;
+    };
+
+    setInterval(() => {
+      this.currentTime = moment().format("HH:mm");
+    }, 700);
+
+    unsplash.photos
+      .getRandomPhoto({ username: "naoufal" })
+      .then(toJson)
+      .then(json => {
+        console.log("json", json);
+        this.bgImg=json.urls.regular
+      });
   }
 };
 </script>
 
 <style lang='less'>
-  @import 'assets/css/global.css'; /*引入公共样式*/
-  @import 'App.less';
+@import "assets/css/global.css"; /*引入公共样式*/
+@import "App.less";
 </style>
