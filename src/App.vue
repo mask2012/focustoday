91023703db017fe3fc5c6b89f19616bf93d9cd46
mask2012 <156544634@qq.com>
2020-05-08 10:30:57 +0800

    调整页面结构

--- src/App.vue
+++ src/App.vue
@@ -12,32 +12,30 @@
     </div>
 
     <div class="region region_center_below">
-      <div class="todo_list_holder">
-        <div class="todo_list">
-          <div class="todo_input">
-            <h3>What is your main focus for today?</h3>
-            <input
-              type="text"
-              autocomplete="off"
-              maxlength="35"
-              v-model="newTodo"
-              @keyup.enter="addTodo"
-            />
-          </div>
-          <div class="todo_scroll" v-show="todos.length" v-cloak>
-            <ul :style="'height:'+listHeight+'px'">
-              <li
-                v-for="todo in todos"
-                :key="todo.id"
-                :class="{ li_done: todo.completed}"
-                :style="'height:'+liHeight+'px; line-height:'+liHeight+'px; font-size:'+liFontSize+'px'"
-                @click="toggleTodo(todo)"
-              >
-                <span>{{ todo.title }}</span>
-                <i class="ico_destroy" @click="removeTodo(todo)"></i>
-              </li>
-            </ul>
-          </div>
+      <div class="todo_list">
+        <div class="todo_input">
+          <h3>What is your main focus for today?</h3>
+          <input
+            type="text"
+            autocomplete="off"
+            maxlength="35"
+            v-model="newTodo"
+            @keyup.enter="addTodo"
+          />
+        </div>
+        <div class="todo_scroll" v-show="todos.length" v-cloak>
+          <ul :style="'height:'+listHeight+'px'">
+            <li
+              v-for="todo in todos"
+              :key="todo.id"
+              :class="{ li_done: todo.completed}"
+              :style="'height:'+liHeight+'px; line-height:'+liHeight+'px; font-size:'+liFontSize+'px'"
+              @click="toggleTodo(todo)"
+            >
+              <span>{{ todo.title }}</span>
+              <i class="ico_destroy" @click="removeTodo(todo)"></i>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -65,30 +63,19 @@ var todoStorage = {
 };
 
 import moment from "moment";
-import Unsplash, { toJson } from "unsplash-js";
-import fetch from "node-fetch";
-global.fetch = fetch;
 
-const unsplash = new Unsplash({
-  accessKey: "Hr-gc8bEf0AnAC0XRo1WUNvP9oCw4A9ddAOdhYzJ674",
-  secret: "44WWENgCIrtD-H-y1xTPK90IaN_vaTeihYxpgoJBD_0",
-  headers: {
-    "X-Custom-Header": "mask2012"
-  },
-  timeout: 5000 // values set in ms
-});
-// unsplash.users.profile("naoufal")
-//   .catch(err => {
-//     // Your flawless error handling code
-//     console.log('unsplash error goes here',err);
-//   });
+// import Unsplash, { toJson } from "unsplash-js";
+// import fetch from "node-fetch";
+// global.fetch = fetch;
 
-// unsplash.search.photos("dogs", 1, 10, { orientation: "portrait" })
-//   .then(toJson)
-//   .then(json => {
-//     console.log('json',json);
-//     // Your code
-//   });
+// const unsplash = new Unsplash({
+//   accessKey: "Hr-gc8bEf0AnAC0XRo1WUNvP9oCw4A9ddAOdhYzJ674",
+//   secret: "44WWENgCIrtD-H-y1xTPK90IaN_vaTeihYxpgoJBD_0",
+//   headers: {
+//     "X-Custom-Header": "mask2012"
+//   },
+//   timeout: 5000 // values set in ms
+// });
 
 export default {
   name: "App",
@@ -99,10 +86,11 @@ export default {
     return {
       todos: todoStorage.fetch(), //todo全集
       newTodo: "", //新的todo
-      listHeight: document.documentElement.clientHeight * 0.2,
-      liHeight: 0,
-      liFontSize: 10,
-      currentTime: moment().format("HH:mm"),
+
+      listHeight: 0, //todo list整体高度
+      liHeight: 0, //todo list li 高度
+      liFontSize: 10, //li里边字号
+      currentTime: "", //时间
       bgImg: ""
     };
   },
@@ -113,7 +101,7 @@ export default {
         this.liHeight = this.listHeight / todos.length;
         this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
         this.liFontSize =
-          this.liFontSize > 40 ? 40 + this.liFontSize / 10 : this.liFontSize;
+          this.liFontSize > 30 ? 30 + this.liFontSize / 14 : this.liFontSize;
       },
       deep: true
     }
@@ -138,34 +126,37 @@ export default {
     },
     toggleTodo: function(todo) {
       const currentIndex = this.todos.indexOf(todo);
+      if (currentIndex == -1) {
+        return;
+      }
       this.todos[currentIndex].completed = !this.todos[currentIndex].completed;
-    }
-  },
-  mounted() {
-    this.liHeight = this.listHeight / this.todos.length;
-    this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
-    this.liFontSize =
-      this.liFontSize > 40 ? 40 + this.liFontSize / 10 : this.liFontSize;
-
-    window.onresize = () => {
+    },
+    calculateHeight() {
       this.listHeight = document.documentElement.clientHeight * 0.2;
       this.liHeight = this.listHeight / this.todos.length;
       this.liFontSize = (this.listHeight * 0.8) / this.todos.length;
       this.liFontSize =
-        this.liFontSize > 40 ? 40 + this.liFontSize / 10 : this.liFontSize;
+        this.liFontSize > 30 ? 30 + this.liFontSize / 14 : this.liFontSize;
+    }
+  },
+  mounted() {
+    this.calculateHeight();
+
+    window.onresize = () => {
+      this.calculateHeight();
     };
 
     setInterval(() => {
       this.currentTime = moment().format("HH:mm");
     }, 700);
 
-    unsplash.photos
-      .getRandomPhoto({ username: "naoufal" })
-      .then(toJson)
-      .then(json => {
-        console.log("json", json);
-        this.bgImg = json.urls.regular;
-      });
+    // unsplash.photos
+    //   .getRandomPhoto({ username: "naoufal" })
+    //   .then(toJson)
+    //   .then(json => {
+    //     console.log("json", json);
+    //     this.bgImg = json.urls.regular;
+    //   });
   }
 };
 </script>
